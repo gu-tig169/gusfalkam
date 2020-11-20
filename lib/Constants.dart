12cd37b6b5e3c09cdf50ec4fdb6e5d070eab5c05
amandafalken <73556606+amandafalken@users.noreply.github.com>
2020-11-20 17:16:33 +0100

    steg 2

---
+++ lib/Constants.dart
@@ -0,0 +1,17 @@
+//För drop down menyn.
+
+class Constants {
+  static const String done = 'done';
+  static const String undone = 'undone';
+  static const String all = 'all';
+
+  static const List<String> choices = <String>[
+    done,
+    undone,
+    all,
+  ];
+}
+
+class Filter {
+  static String show = 'all';
+}
